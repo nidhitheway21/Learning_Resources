aead89f77d3569c55d2919b3fbb109765adae02b
DivasGupta7 <Dvsmathur7050@gmail.com>
2021-10-08 07:36:04 +0530

    Added EvenOddd_check program in c++

---
+++ Basic Arithmetic/EvenOdd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   
+    int n;cin>>n;
+    if(n%2==0)
+    cout<<"EVEN NUMBER"<<endl;
+    else
+    cout<<"ODD NUMBER"<<endl;
+    return 0;
+}
